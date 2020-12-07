6a597347d6a371f536c221c6c7eca112fb5e02a7
david espinal <despinal0425@gmail.com>
2020-12-07 14:50:34 -0500

    added next and prev to go to the next 10 reports or back to last reports

--- report/redirect.js
+++ report/redirect.js
@@ -7,29 +7,12 @@ const reportUrl = `https://api.reliefweb.int/v1/reports?appname=apidoc&query[val
 
 let response = async function () { 
   await axios.get(reportUrl).then(res => {
-    console.log(res.data);
+    console.log(res.data.totalCount);
 
     const reportData = res.data.data
     for (let i = 0; i < reportData.length; i++) { 
       document.getElementById('title_section').innerHTML += `<li id = ${reportData[i].id}>${reportData[i].fields.title}</li>`
     }
-
-
-    let newA2 = document.createElement('a');
-    newA2.setAttribute("href", '#')
-    newA2.textContent = "prev";
-    document.getElementById('info').appendChild(newA2);
-    
-    let newA1 = document.createElement('a');
-    newA1.setAttribute("href", '#')
-    newA1.textContent = "next";
-    document.getElementById('info').appendChild(newA1);
-
-
-
-    
-
-
   }).catch(err => { 
     console.log(err)
   })
@@ -38,6 +21,8 @@ let response = async function () {
 response();
 
 
+
+//When a title is clicked, the either an image or body text will appeared
 document.getElementById('title_section').addEventListener('click', async (e) => { 
   console.log(e.target);
   let reportId = e.target.id;
@@ -46,14 +31,17 @@ document.getElementById('title_section').addEventListener('click', async (e) => 
   let divElement = document.querySelector('div');
   divElement.innerHTML = "";
 
+
+  
+  let newAEle = document.createElement('a');
+
   try {
     const responseTwo = await axios.get(`https://api.reliefweb.int/v1/reports/${reportId}`);
-    console.log(responseTwo.data.data[0])
+    console.log(responseTwo.data.data[0].fields['body-html'])
 
     let responseTitle = responseTwo.data.data[0].fields.title
-    let responseBody = responseTwo.data.data[0].fields.body
+    let responseBody = responseTwo.data.data[0].fields['body-html']
     
-
 
     let newHeaderElement = document.createElement('h3')
     newHeaderElement.textContent = `${responseTitle}`
@@ -66,18 +54,84 @@ document.getElementById('title_section').addEventListener('click', async (e) => 
       divElement.appendChild(newImageElement);
     } else {
       let newParElement = document.createElement('p');
-      newParElement.textContent = `${responseBody}`
+      newParElement.innerHTML = `${responseBody}`
       divElement.appendChild(newParElement);
     }
    
-    
-
   } catch (e) { 
     console.log(e);
   }
-
-  
 })
 
 
 
+
+
+//Click next or previous and you will get fresh 10 new reports
+let num = 0;
+
+//For prev
+let newA2 = document.createElement('a');
+newA2.setAttribute("href", '#')
+newA2.setAttribute("id", 'prevTitle')
+newA2.textContent = "";
+document.getElementById('info').appendChild(newA2);
+
+document.getElementById('prevTitle').addEventListener('click', async (e) => {
+  
+  try {
+    num -= 10;
+    console.log(num);
+
+    const responseThree = await axios.get(`https://api.reliefweb.int/v1/reports?appname=apidoc&query[value]=${searchReport}&offset=${num}&limit=10`);
+    console.log(responseThree.data);
+
+    if (num >= 0) {
+      console.log(num);
+      document.getElementById('title_section').innerHTML = ""
+      const reponseTitleTwo = responseThree.data.data
+      for (let i = 0; i < reponseTitleTwo.length; i++) {
+        document.getElementById('title_section').innerHTML += `<li id = ${reponseTitleTwo[i].id}>${reponseTitleTwo[i].fields.title}</li>`
+      }
+    }
+    if (num === 0) { 
+      newA2.textContent = "";
+    }
+  
+  } catch (e) { 
+    console.log(e)
+    num = 0;
+  }
+})
+
+
+//For next
+let newA1 = document.createElement('a');
+newA1.setAttribute("href", '#')
+newA1.setAttribute("id", 'nextTitle')
+newA1.textContent = "next";
+document.getElementById('info').appendChild(newA1);
+
+document.getElementById('nextTitle').addEventListener('click', async (e) => {
+  
+  try {
+    newA2.textContent = "prev";
+    num += 10;
+    console.log(num);
+    
+
+    const responseThree = await axios.get(`https://api.reliefweb.int/v1/reports?appname=apidoc&query[value]=${searchReport}&offset=${num}&limit=10`);
+    console.log(responseThree.data);
+
+    if (num <= responseThree.data.totalCount) {
+      document.getElementById('title_section').innerHTML = ""
+      const reponseTitleTwo = responseThree.data.data
+      for (let i = 0; i < reponseTitleTwo.length; i++) {
+        document.getElementById('title_section').innerHTML += `<li id = ${reponseTitleTwo[i].id}>${reponseTitleTwo[i].fields.title}</li>`
+      }
+    }
+  
+  } catch (e) { 
+    console.log(e)
+  }
+})
